9716b62ab8a7be95cf2e1cc2bad7206ee075e22f
Anton Mushnin <anton.mushnin@gmail.com>
2022-04-29 13:10:46 +0300

    bind

--- Controller.js
+++ Controller.js
@@ -8,13 +8,14 @@ class Controller {
     this.currentCardIndex = 0;
   }
 
+
   run() {
     // Просим экземпляр класса модели прочитать папку со всеми темами и составить меню.
     // Попутно передаем метод контроллера this.printTopicsController,
     // так как нам нужно отправить сформинованное меню на вывод в экземпляр класса view
     // после того, как завершится асинхронная операция чтения папки
     // Здесь this.printTopicsController — является callback'ом  
-    this.model.readTopics(this.printTopicsController)
+    this.model.readTopics(this.printTopicsController.bind(this))
 
   }
 
