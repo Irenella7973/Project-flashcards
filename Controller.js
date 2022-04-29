2821151b911c5c2514b5f3a3de546c6569d2d6ca
Anton Mushnin <anton.mushnin@gmail.com>
2022-04-29 11:44:33 +0300

    play

--- Controller.js
+++ Controller.js
@@ -2,6 +2,7 @@ class Controller {
   constructor(model, view) {
     this.model = model
     this.view = view
+    this.currentCardIndex = 0;
   }
 
   run() {
@@ -11,14 +12,30 @@ class Controller {
     // после того, как завершится асинхронная операция чтения папки
     // Здесь this.printTopicsController — является callback'ом  
     this.model.readTopics(this.printTopicsController)
+
   }
 
-  printTopicsController(topicsMenu) {
+  async printTopicsController(topicsMenu) {
+    for (topic in this.model.topics) {
+      this.view.showTopic(topic);
+    }
+    const currentTopic = await this.view.getTopic()
+    startQuiz(topic)
     // Тут нужно попросить экземпляр класса view вывести меню пользователю, 
     // а также дождаться ответа последнего
   }
 
-  
+  async step(index) {
+    const answer = await this.view.askQuestion(model.cards[index]);
+  }
+
+  async play(topic) {
+    const cards = this.model.getTopic(topic);
+    do {
+      const step = await this.step(cards[currentCard]);
+      currentCard += 1;
+    } while(currentCard < cards.length)
+  } 
 }
 
 module.exports = Controller
