89805a6219225f27316af211e1dae762206ab532
Anton Mushnin <anton.mushnin@gmail.com>
2022-04-29 14:28:54 +0300

    going on

--- Controller.js
+++ Controller.js
@@ -1,5 +1,7 @@
+// const { require } = require('yargs');
 const Model = require('./mockModel');
 const View = require('./View');
+// const Card = require('./Card');
 
 class Controller {
   constructor(model, view) {
@@ -9,11 +11,6 @@ class Controller {
   }
 
   run() {
-    // Просим экземпляр класса модели прочитать папку со всеми темами и составить меню.
-    // Попутно передаем метод контроллера this.printTopicsController,
-    // так как нам нужно отправить сформинованное меню на вывод в экземпляр класса view
-    // после того, как завершится асинхронная операция чтения папки
-    // Здесь this.printTopicsController — является callback'ом
     this.model.readTopics(this.printTopicsController.bind(this));
   }
 
@@ -24,18 +21,22 @@ class Controller {
     }
     const currentTopic = await this.view.getTopic();
     this.play(currentTopic);
-    // Тут нужно попросить экземпляр класса view вывести меню пользователю,
-    // а также дождаться ответа последнего
   }
 
   async play(topic) {
     const cards = this.model.getTopic(topic);
+    let score = 0;
+
     console.log(cards);
     let answer;
+
     do {
-      answer = await this.view.askQuestion(cards[this.currentCardIndex].question);
+      const card = cards[this.currentCardIndex];
+      answer = await this.view.askQuestion(card.question);
+      if (card.isRight()) { score += 100 / cards.length; }
       this.currentCardIndex += 1;
     } while (this.currentCardIndex < cards.length && answer !== ':q');
+    console.log(score);
   }
 }
 
--- Model.js
+++ Model.js
@@ -1,4 +1,4 @@
-let fs = require('fs').promises;
+const fs = require('fs').promises;
 const { isPromise } = require('util/types');
 
 class Model {
@@ -7,26 +7,22 @@ class Model {
     this.topics = [];
     this.files = ['nighthawk_flashcard_data.txt', 'otter_flashcard_data.txt', 'raccoon_flashcard_data.txt'];
     this.path = path;
-
   }
 
   async readTopics() {
     const topicsDir = await fs.readdir('./topics', 'utf-8');
     for (let i = 0; i < this.files.length; i++) {
-      const data = await fs.readFile(`./topics/${this.files[i]}`, 'utf-8').then((data) => this.createTopic(data));  
+      const data = await fs.readFile(`./topics/${this.files[i]}`, 'utf-8').then((data) => this.createTopic(data));
     }
     //  Я не понял где именно запустить функцию 'f'
   }
 
-
-  
   createTopic(data) {
-  
-  }
 
+  }
 }
 const ttt = new Model();
 ttt.readTopics();
 
-//console.log(Model.readTopics());
+// console.log(Model.readTopics());
 module.exports = Model;
