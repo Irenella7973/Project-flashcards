5d48e9b20d01889b69c146b5645de55b72170586
Irina Tikhonova <102436907+Irenella7973@users.noreply.github.com>
2022-04-29 12:13:47 +0300

    Class Model

--- Model.js
+++ Model.js
@@ -1,7 +1,28 @@
+let fs = require('fs');
+const { isPromise } = require('util/types');
+
 class Model {
-  constructor() {
+  constructor(path) {
+    this.cards = [];
+    this.topics = [];
+    this.files = ['nightawk_flashcard_data.txt', 'otter_flashcard_data.txt', 'raccoon_flashcard_data.txt'];
+    this.path = path;
 
   }
+
+  async function readTopics(f) {
+    for (let i = 0; i < this.files.length; i++) {
+      const data = await fs.readFile(this.files[i])
+    }
+    console.log(data)  
+  }
+
+
+  
+  createTopic(data) {
+  
+  }
+
 }
 
-module.exports = Model
+module.exports = Model;
