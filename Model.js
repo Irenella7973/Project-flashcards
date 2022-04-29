d155dfb3b6ad71844e5c650e7505cfe4b8682410
Bagavdin <bagabruklin@gmail.com>
2022-04-29 13:49:36 +0300

    readTopics

--- Model.js
+++ Model.js
@@ -1,20 +1,21 @@
-let fs = require('fs');
+let fs = require('fs').promises;
 const { isPromise } = require('util/types');
 
 class Model {
   constructor(path) {
     this.cards = [];
     this.topics = [];
-    this.files = ['nightawk_flashcard_data.txt', 'otter_flashcard_data.txt', 'raccoon_flashcard_data.txt'];
+    this.files = ['nighthawk_flashcard_data.txt', 'otter_flashcard_data.txt', 'raccoon_flashcard_data.txt'];
     this.path = path;
 
   }
 
-  async function readTopics(f) {
+  async readTopics() {
+    const topicsDir = await fs.readdir('./topics', 'utf-8');
     for (let i = 0; i < this.files.length; i++) {
-      const data = await fs.readFile(this.files[i])
+      const data = await fs.readFile(`./topics/${this.files[i]}`, 'utf-8').then((data) => this.createTopic(data));  
     }
-    console.log(data)  
+    //  Я не понял где именно запустить функцию 'f'
   }
 
 
@@ -24,5 +25,8 @@ class Model {
   }
 
 }
+const ttt = new Model();
+ttt.readTopics();
 
+//console.log(Model.readTopics());
 module.exports = Model;
