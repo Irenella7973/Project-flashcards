f4ad8768f341041b89966a809027133f951cfb15
Anton Mushnin <anton.mushnin@gmail.com>
2022-04-29 16:25:47 +0300

    refact, reading dir

--- Model.js
+++ Model.js
@@ -7,23 +7,21 @@ class Model {
   constructor(path) {
     this.cards = [];
     this.topics = [];
-    this.files = ['nighthawk_flashcard_data.txt', 'otter_flashcard_data.txt', 'raccoon_flashcard_data.txt'];
     this.path = path;
   }
 
   async readTopics(f) {
     console.log('rt');
-    const topicsDir = await fs.readdir('./topics', 'utf-8');
-    for (let i = 0; i < this.files.length; i++) {
-      const data = await fs.readFile(`./topics/${this.files[i]}`, 'utf-8').then((data) => this.createTopic(data));
+    const files = await fs.readdir('./topics', 'utf-8');
+    for (let i = 0; i < files.length; i++) {
+      const data = await fs.readFile(`./topics/${files[i]}`, 'utf-8')
+      this.createTopic(data);
     }
     f();
   }
 
   createTopic(data) {
-
-    let arrayData1 = data.split('\n');
-    let arrayData = arrayData1.filter((el) => el !== '');
+    let arrayData = data.split('\n').filter((el) => el !== '');
     this.topics.push(arrayData[0]);
     for (let i = 1; i < arrayData.length; i += 2) {
       let newCards = new Card(arrayData[0], arrayData[i], arrayData[i + 1]);
